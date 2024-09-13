31da8cad9afd784944582e587c461e78b4055d9d
Kyle Johnson <kyjohnson09@gmail.com>
2024-09-13 17:55:26 -0500

    Initial Commit

---
+++ OpenTok/OpenTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTokApp.swift
+//  OpenTok
+//
+//  Created by Kyle Johnson on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
