05855849cac05cf0a9c4e0eac6aa8c8bd54044d0
Kyle Johnson <kyjohnson09@gmail.com>
2024-09-17 18:03:02 -0500

    Add basic script

--- OpenTok Extension/Resources/content.js
+++ OpenTok Extension/Resources/content.js
@@ -1,7 +1,11 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+// Get the current URL
+const url = new URL(window.location.href);
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+// Check if the hostname is 'www.tiktok.com', the pathname matches the expected pattern, and there are query parameters
+if (url.hostname === 'www.tiktok.com' && /^\/@[^/]+\/(video|photo)\/\d+/.test(url.pathname) && url.search) {
+    // Construct the new URL without query parameters
+    const newUrl = `${url.origin}${url.pathname}`;
+    
+    // Redirect to the new URL
+    window.location.replace(newUrl);
+}
