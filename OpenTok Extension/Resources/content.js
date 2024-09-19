c6a91d509aeb8473a9369fbe0ef458a46d9f537e
Kyle Johnson <kyjohnson09@gmail.com>
2024-09-18 20:29:31 -0500

    Update js, remove smart app banner

--- OpenTok Extension/Resources/content.js
+++ OpenTok Extension/Resources/content.js
@@ -1,11 +1,17 @@
 // Get the current URL
 const url = new URL(window.location.href);
 
-// Check if the hostname is 'www.tiktok.com', the pathname matches the expected pattern, and there are query parameters
-if (url.hostname === 'www.tiktok.com' && /^\/@[^/]+\/(video|photo)\/\d+/.test(url.pathname) && url.search) {
-    // Construct the new URL without query parameters
-    const newUrl = `${url.origin}${url.pathname}`;
+// Check if the URL is for a TikTok video or photo slideshow
+if (url.hostname === 'www.tiktok.com' && /^\/@[^/]+\/(video|photo)\/\d+/.test(url.pathname)) {
+    // Check for query parameters (which prevent video playback)
+    if (url.search) {
+        // Construct a new URL without query parameters
+        const newUrl = `${url.origin}${url.pathname}`;
+        
+        // Redirect to the new URL
+        window.location.replace(newUrl);
+    }
     
-    // Redirect to the new URL
-    window.location.replace(newUrl);
+    // Remove smart app banner
+    document.querySelector('meta[name="apple-itunes-apps"]')?.remove();
 }
