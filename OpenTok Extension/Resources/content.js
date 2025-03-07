8b2104ebeb768f1014206d36441278c034debd39
Kyle Johnson <kyjohnson09@gmail.com>
2025-03-06 19:04:08 -0600

    Add casual review prompt

--- OpenTok Extension/Resources/content.js
+++ OpenTok Extension/Resources/content.js
@@ -12,11 +12,14 @@ if (url.hostname === 'www.tiktok.com' && /^\/@[^/]*\/(video|photo)\/\d+/.test(ur
         window.location.replace(newUrl);
     }
     
-    // Remove smart app banner and automatically close dialog boxes
     document.addEventListener('DOMSubtreeModified', () => {
+        // Remove smart app banner and automatically close dialog boxes
         document.querySelector('meta[name="apple-itunes-app"]')?.remove();
         document.querySelector('button[class*="close-button"]')?.click();
         document.querySelector('span[data-e2e*="launch-popup-close"]')?.click();
+        
+        // Attempt to insert casual review prompt
+        insertMessageUnderWatchAgain();
     });
     
     // Force the "Watch again" button to restart the video
@@ -40,4 +43,67 @@ if (url.hostname === 'www.tiktok.com' && /^\/@[^/]*\/(video|photo)\/\d+/.test(ur
             window.location.replace(link.href);
         }
     });
+}
+
+function insertMessageUnderWatchAgain() {
+    // Exclude from setup video
+    const setupVideoId = "/video/6876424179084709126";
+    if (url.pathname.includes(setupVideoId)) return;
+    
+    // Exclude if user has already reviewed the current version
+    const currentVersion = chrome.runtime.getManifest().version;
+    if (currentVersion === localStorage.getItem("OpenTok-lastReviewedVersion")) return;
+    
+    // Do not reinsert if message is already present
+    const messageId = "opentok-message";
+    if (document.getElementById(messageId)) return;
+    
+    // Do not insert if 'Watch again' container cannot be found
+    const targetElement = document.querySelector('div[class*="DivSwiperList"]');
+    if (!targetElement) return;
+    
+    const message = createMessageElement(messageId);
+    targetElement.appendChild(message);
+}
+
+function createMessageElement(messageId) {
+    const message = document.createElement("div");
+    message.id = messageId;
+    
+    Object.assign(message.style, {
+        width: "100%",
+        textAlign: "center",
+        padding: "25px",
+        fontSize: "13px",
+        fontFamily: "Arial, Tahoma, sans-serif",
+        position: "absolute",
+        bottom: "0",
+        left: "0"
+    });
+    
+    const link = createReviewLink();
+    message.appendChild(link);
+    return message;
+}
+
+function createReviewLink() {
+    const link = document.createElement("a");
+    link.href = "opentok://review";
+    Object.assign(link.style, { color: "white", textDecoration: "none" });
+    
+    const plainText = document.createElement("span");
+    const underlinedText = document.createElement("span");
+    underlinedText.style.textDecoration = "underline";
+    
+    plainText.textContent = "Enjoying OpenTok?";
+    underlinedText.textContent = "Help spread the word!";
+    link.append(plainText, " ", underlinedText);
+    
+    link.addEventListener("click", () => {
+        localStorage.setItem("OpenTok-lastReviewedVersion", chrome.runtime.getManifest().version);
+        plainText.textContent = "Thank you ❤️";
+        underlinedText.textContent = "";
+    });
+    
+    return link;
 }
--- OpenTok/OpenTokApp.swift
+++ OpenTok/OpenTokApp.swift
@@ -5,13 +5,72 @@
 //  Created by Kyle Johnson on 9/13/24.
 //
 
+import StoreKit
 import SwiftUI
 
 @main
 struct OpenTokApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentScene()
         }
     }
+}
+
+struct ContentScene: View {
+    @Environment(\.requestReview) private var requestReview
+    @State var isReviewSheetPresented = false
+    
+    var body: some View {
+        ContentView()
+            .onOpenURL { url in
+                switch url.host {
+                case "review":
+                    Task { @MainActor in
+                        try await Task.sleep(for: .seconds(0.25))
+                        requestReview()
+                        try await Task.sleep(for: .seconds(0.75))
+                        isReviewSheetPresented = true
+                    }
+                default:
+                    break
+                }
+            }
+            .sheet(isPresented: $isReviewSheetPresented) {
+                ReviewSheetView()
+            }
+    }
+}
+
+struct ReviewSheetView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+                Text("OpenTok v\(appVersion)")
+                    .font(.headline)
+                Spacer()
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(Color(UIColor.lightGray))
+                        .imageScale(.large)
+                }
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 20)
+            
+            Text("Leaving a rating or [review](https://itunes.apple.com/app/id6708240044?action=write-review) supports development and makes it easier for new users to discover OpenTok on the App Store.")
+                .font(.callout)
+                .padding(.horizontal, 20)
+        }
+        .presentationDetents([.height(150)])
+    }
+}
+
+#Preview {
+    ContentScene(isReviewSheetPresented: true)
 }
